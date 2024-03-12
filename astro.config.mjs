9a6de80c3bb3bfce142983bd5b7e5c53801145bf
Karl Edenstål <karl.edenstal@rhyme.se>
2024-03-12 21:01:45 +0100

    SSR for handling forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
